2384b2d4cc99a678a4915561ad586491e4eb108b
Rafael Santos <rafa.05125@gmail.com>
2023-01-17 19:29:32 -0300

    College - POST

--- college/src/main/java/com/rafaelsantos/college/resources/CollegeResource.java
+++ college/src/main/java/com/rafaelsantos/college/resources/CollegeResource.java
@@ -1,5 +1,6 @@
 package com.rafaelsantos.college.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaelsantos.college.domain.College;
 import com.rafaelsantos.college.domain.dto.CollegeDTO;
@@ -33,5 +37,14 @@ public class CollegeResource {
 	public ResponseEntity<CollegeDTO> findById(@PathVariable String id){
 		College obj = service.findById(id);
 		return ResponseEntity.ok().body(new CollegeDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<CollegeDTO> insert(@RequestBody CollegeDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- college/src/main/java/com/rafaelsantos/college/services/CollegeService.java
+++ college/src/main/java/com/rafaelsantos/college/services/CollegeService.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaelsantos.college.domain.College;
+import com.rafaelsantos.college.domain.dto.CollegeDTO;
 import com.rafaelsantos.college.repositories.CollegeRepository;
 import com.rafaelsantos.college.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +25,16 @@ public class CollegeService {
 	public College findById(String id) {
 		Optional<College> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	@Transactional
+	public CollegeDTO insert(CollegeDTO dto) {
+		College entity = new College();
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		
+		entity = repository.save(entity);
+		
+		return new CollegeDTO(entity);
 	}
 }

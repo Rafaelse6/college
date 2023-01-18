9a918acbed092419edbf421c2921890e56de1d65
Rafael Santos <rafa.05125@gmail.com>
2023-01-18 05:59:21 -0300

    Department

--- college/src/main/java/com/rafaelsantos/college/config/Instantiation.java
+++ college/src/main/java/com/rafaelsantos/college/config/Instantiation.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.rafaelsantos.college.domain.College;
+import com.rafaelsantos.college.domain.Department;
 import com.rafaelsantos.college.repositories.CollegeRepository;
+import com.rafaelsantos.college.repositories.DepartmentRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
@@ -15,15 +17,24 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private CollegeRepository repository;
 	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		repository.deleteAll();
+		departmentRepository.deleteAll();
 		
 		College hero = new College(null, "Boku no Hero College", "hero@gmail.com");
 		College usb = new College(null, "USB College", "usb@gmail.com");
 		College mnk = new College(null, "MNK College", "mnk@gmail.com");
 		
+		Department department1 = new Department(null, "History", "historydepartment@gmail.com", "History department", hero);
+		Department department2 = new Department(null, "Science", "sciencedepartment@gmail.com", "Science department", usb);
+		Department department3 = new Department(null, "Math", "mathdepartment@gmail.com", "Math department", mnk);
+		
 		repository.saveAll(Arrays.asList(hero, usb, mnk));
+		departmentRepository.saveAll(Arrays.asList(department1, department2, department3));
 	}
 }
---
+++ college/src/main/java/com/rafaelsantos/college/repositories/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelsantos.college.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.rafaelsantos.college.domain.Department;
+
+public interface DepartmentRepository extends MongoRepository<Department, String> {
+
+}
